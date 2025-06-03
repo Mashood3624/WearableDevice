7fc65f292e5a729a3a6fde64b06245f953690764
Mashood <mashood3624@gmail.com>
2025-06-03 14:56:49 +0400

    Update astro.config.mjs

--- academic-project-astro-template-main/astro.config.mjs
+++ academic-project-astro-template-main/astro.config.mjs
@@ -9,7 +9,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/WearableDevice/academic-project-astro-template-main',
+  base: '/WearableDevice/',
   integrations: [tailwind(), icon(), mdx(), react(), svelte()],
   markdown: {
     shikiConfig: {
